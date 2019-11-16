7614abf23a0b2502acc43f4851b76a01529aa7b6
BogdanKiriluyk <21gunsprojektX@gmail.com>
2019-11-16 18:51:32 +0200

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import TabComponent from '../components/TabComponent'
+import Footer from "../components/Footer";
+
+class Main extends React.Component {
+	render() {
+		return (
+			<div>
+				<Header />
+				<TabComponent />
+				<Footer />
+			</div>
+		);
+	}
+}
+
+export default Main;
